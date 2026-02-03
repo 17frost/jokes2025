ff97621617a57c903f52ed2413f4c7799feeac85
17frost <144569847+17frost@users.noreply.github.com>
2026-02-03 19:34:22 +0000

    will add sorry message later

--- alpha.py
+++ alpha.py
@@ -25,4 +25,57 @@
     
 # questions()
 
+#COPY FOR TESTING
+#Joke Database for each line
+joke_starter=("Knock Knock", "Knock Knock", "Knock Knock")
+joke_2nd_line=("Calder", "Tank", "Broken pencil")
+joke_punchline=("Calder police - I've been robbed!", "You are welcome! ", "Nevermind it's pointless! ")
+#Defining joke type in Database with input response
+joke_db = {
+    "robbers": [joke_starter[0], joke_2nd_line[0], joke_punchline[0]],
+    "tanks": [joke_starter[1], joke_2nd_line[1], joke_punchline[1]],
+    "pencils": [joke_starter[2], joke_2nd_line[2], joke_punchline[2]],
+}
+#Getting input from user asking for which joke user would like to hear in the database
+
+def ask_joke():
+    subject = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
+
+    joke_lines = joke_db[subject]
+
+    # Indexing joke_db and printing them in respect to user input. Selects the joke from the Joke_2nd_line options.
+
+    if subject not in joke_db:
+         input("Sorry, I only know jokes about robbers, tanks, or pencils. Would u like to add one yourself? (Y/n)")
+    elif subject in joke_db:
+        for i, line in enumerate(joke_lines):
+            print(f"{line}")
+            input()
+    return
+ask_joke()
+
+#Restarts the ask_joke prompt to continue loop through joke database
+def start_again():
+    start_over = input("Do you want to hear another joke? (Y/n) ".lower())
+    if start_over == "y":
+        ask_joke()
+        start_again()
+    elif start_over == "n":
+        print("bye!")
+        return
+    
+start_again()
+
+ 
+ def punchline()
+ 
+ 
+ 
+ 
+ def new_joke(**name):
+    input(f"what would you like to call this joke?")
+    new_joke = []
+        for key, value in name.items():
+            new_joke.append(value)
+        return new_joke
 
--- beta.py
+++ beta.py
@@ -1,63 +1,50 @@
-#Joke Database
+#Joke Database for each line
 joke_starter=("Knock Knock", "Knock Knock", "Knock Knock")
 joke_2nd_line=("Calder", "Tank", "Broken pencil")
 joke_punchline=("Calder police - I've been robbed!", "You are welcome! ", "Nevermind it's pointless! ")
-
-#Defining joke type in Database
+#Defining joke type in Database with input response
 joke_db = {
     "robbers": [joke_starter[0], joke_2nd_line[0], joke_punchline[0]],
     "tanks": [joke_starter[1], joke_2nd_line[1], joke_punchline[1]],
     "pencils": [joke_starter[2], joke_2nd_line[2], joke_punchline[2]],
 }
-#Getting input from user asking for which joke they would like to hear in the database
+#Getting input from user asking for which joke user would like to hear in the database
 
 def ask_joke():
     subject = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
 
     joke_lines = joke_db[subject]
-
-    #Taking lines out of jokes and printing them with indexing from user input. Selects the joke from the Joke_2nd_line options.
+    
+    print(joke_lines)
+    # Indexing joke_db and printing them in respect to user input. Selects the joke from the Joke_2nd_line options.
 
     if subject not in joke_db:
-        print("Sorry, I only know jokes about robbers, tanks, or pencils.")
-    elif subject in joke_db:
+        input("Sorry, I only know jokes about robbers, tanks, or pencils. Would u like to add one yourself? (Y/n)")
+        print(joke_lines)
+    for subject in joke_db:
         for i, line in enumerate(joke_lines):
             print(f"{line}")
             input()
-    return
+        return
+ask_joke()
 
 #Restarts the ask_joke prompt to continue loop through joke database
-start_over = input("Do you want to hear another joke? (Y/n) ")
-if start_over == "yes":
-    ask_joke
-elif:
-print("bye!")
+def start_again():
+    start_over = input("Do you want to hear another joke? (Y/n) ".lower())
+    if start_over == "y":
+        ask_joke()
+        start_again()
+    elif start_over == "n":
+        print("bye!")
+        return
+    
+start_again()
 
     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-# def tell_joke():
-#     print(f"Do you want to hear a joke about robbers, tanks, or pencils?"
+#  def new_joke(decision):
+#     print(f"what would you like to call this joke?"
           
 #     if subject not in joke_db:
 #         print("Sorry, I only know jokes about robbers, tanks, or pencils!")
